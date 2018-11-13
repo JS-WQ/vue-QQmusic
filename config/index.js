7653ffc4647664bd2b1508a0551762c7928db5d4
unknown <552410301@qq.com>
2018-11-13 22:26:53 +0800

    预览

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-music/dist',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
